8cefd5f007ef03f50dfe591c002c95ef56adb568
JohnRuanee <johnruanee@gmail.com>
2023-02-04 14:19:52 -0500

    h
    h

--- frontend/dshare/src/App.js
+++ frontend/dshare/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Auth from "./Auth"
 import Auth from "./components/Auth"
 import Display from "./components/Display"
 
